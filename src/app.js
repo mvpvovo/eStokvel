0b0e89271c2d27c236229c006762247f1a634378
mvpvovo <mvpvovo@gmail.com>
2026-02-23 19:14:16 +0200

    Change server port to 25397

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use('/api/events', require('./routes/events'));
 app.use('/api/alcohol', require('./routes/alcohol'));
 app.use('/api/reports', require('./routes/reports'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 25397;
 
 sequelize.sync({ alter: true }).then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
